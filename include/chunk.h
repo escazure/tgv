9ac8e49b1f02a95bcf5b94d66077ef8815d337d7
escazure <haiper.1453@gmail.com>
2026-08-14 19:58:17 +0200

    Cleaned up terrain.h and chunk.h

--- include/chunk.h
+++ include/chunk.h
@@ -2,77 +2,75 @@
 #include <vector>
 
 class Chunk{
-	public:
-		std::size_t _vertexCount = 0;
+public:
+	std::size_t _vertexCount = 0;
 
-		Chunk(std::size_t xPos, std::size_t zPos, float stepSize, std::size_t chunkSize, std::size_t terrainSize){
-			_chunkSize = chunkSize;
-			_stepSize = stepSize;
-			_terrainSize = terrainSize;
-			_xPos = xPos;
-			_zPos = zPos;
-		}
+	Chunk(std::size_t xPos, std::size_t zPos, float stepSize, std::size_t chunkSize, std::size_t terrainSize){
+		_chunkSize = chunkSize;
+		_stepSize = stepSize;
+		_terrainSize = terrainSize;
+		_xPos = xPos;
+		_zPos = zPos;
+	}
 
-		~Chunk(){
-			if(_VAO)
-				glDeleteVertexArrays(1, &_VAO);
+	~Chunk(){
+		if(_VAO)
+			glDeleteVertexArrays(1, &_VAO);
+		if(_VBO)
+			glDeleteBuffers(1, &_VBO);
+	}
 
-			if(_VBO)
-				glDeleteBuffers(1, &_VBO);
-		}
+	void generate(unsigned int shaderEBO, std::size_t indexCount){
+		_indexCount = indexCount;
+		pGenerateVertices();
 
-		void generate(unsigned int shaderEBO, std::size_t indexCount){
-			_indexCount = indexCount;
-			pGenerateVertices();
+		glCreateVertexArrays(1, &_VAO);
+		glCreateBuffers(1, &_VBO);
 
-			glCreateVertexArrays(1, &_VAO);
-			glCreateBuffers(1, &_VBO);
+		glNamedBufferData(_VBO, _vertices.size() * sizeof(float), _vertices.data(), GL_STATIC_DRAW);
 
-			glNamedBufferData(_VBO, _vertices.size() * sizeof(float), _vertices.data(), GL_STATIC_DRAW);
+		glVertexArrayElementBuffer(_VAO, shaderEBO);
 
-			glVertexArrayElementBuffer(_VAO, shaderEBO);
+		glEnableVertexArrayAttrib(_VAO, 0);
+		glVertexArrayAttribFormat(_VAO, 0, 2, GL_FLOAT, GL_FALSE, 0);
+		glVertexArrayAttribBinding(_VAO, 0, 0);
+		glVertexArrayVertexBuffer(_VAO, 0, _VBO, 0, 2 * sizeof(float));
 
-			glEnableVertexArrayAttrib(_VAO, 0);
-			glVertexArrayAttribFormat(_VAO, 0, 2, GL_FLOAT, GL_FALSE, 0);
-			glVertexArrayAttribBinding(_VAO, 0, 0);
-			glVertexArrayVertexBuffer(_VAO, 0, _VBO, 0, 2 * sizeof(float));
+		_vertices.clear();
+		_vertices.shrink_to_fit();
+	}
 
-			_vertices.clear();
-			_vertices.shrink_to_fit();
-		}
-
-		void draw() const {
-			glBindVertexArray(_VAO);
-			glDrawElements(GL_TRIANGLES, _indexCount, GL_UNSIGNED_INT, 0);
-			glBindVertexArray(0);
-		}
+	void draw() const {
+		glBindVertexArray(_VAO);
+		glDrawElements(GL_TRIANGLES, _indexCount, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
 		
-	private:
-		unsigned int _VAO = 0, _VBO = 0;
-		std::size_t _indexCount = 0;
-		std::vector<float> _vertices;	
+private:
+	unsigned int _VAO = 0, _VBO = 0;
+	std::size_t _indexCount = 0;
+	std::vector<float> _vertices;	
 
-		std::size_t _chunkSize, _terrainSize;		
-		std::size_t _xPos, _zPos;
-		float _stepSize;
+	std::size_t _chunkSize, _terrainSize;		
+	std::size_t _xPos, _zPos;
+	float _stepSize;
 
-		void pGenerateVertices(){
-			int x0 = _xPos * _chunkSize - (_terrainSize * 0.5f);
-			int z0 = _zPos * _chunkSize - (_terrainSize * 0.5f);
+	void pGenerateVertices(){
+		int x0 = _xPos * _chunkSize - (_terrainSize * 0.5f);
+		int z0 = _zPos * _chunkSize - (_terrainSize * 0.5f);
 
-			std::size_t axisVertexCount = std::size_t(_chunkSize / _stepSize) + 1;
-			_vertexCount = axisVertexCount * axisVertexCount;
-			_vertices.resize(_vertexCount * 2);
+		std::size_t axisVertexCount = std::size_t(_chunkSize / _stepSize) + 1;
+		_vertexCount = axisVertexCount * axisVertexCount;
+		_vertices.resize(_vertexCount * 2);
 
-			std::size_t index = 0;
-			for(std::size_t i = 0; i < axisVertexCount; i++){
-				float world_x = x0 + i * _stepSize;
-				for(std::size_t j = 0; j < axisVertexCount; j++){
-					float world_z = z0 + j * _stepSize;
-
-					_vertices[index++] = world_x;	
-					_vertices[index++] = world_z;	
-				}
+		std::size_t index = 0;
+		for(std::size_t i = 0; i < axisVertexCount; i++){
+			float world_x = x0 + i * _stepSize;
+			for(std::size_t j = 0; j < axisVertexCount; j++){
+				float world_z = z0 + j * _stepSize;
+				_vertices[index++] = world_x;	
+				_vertices[index++] = world_z;	
 			}
 		}
+	}
 };
--- include/terrain.h
+++ include/terrain.h
@@ -5,80 +5,80 @@
 #include "chunk.h"
 
 class Terrain {
-	public:
-		int _size;
-		float _stepSize;
-		std::size_t _chunkSize;
-		std::size_t _vertexCount = 0;
-		std::size_t _triangleCount = 0;
+public:
+	int _size;
+	float _stepSize;
+	std::size_t _chunkSize;
+	std::size_t _vertexCount = 0;
+	std::size_t _triangleCount = 0;
 
-		Terrain(){}
-		Terrain(std::size_t size, float stepSize, std::size_t chunkSize){
-			_size = size;
-			_stepSize = stepSize;
-			_chunkSize = chunkSize;
+	Terrain(){}
+	Terrain(std::size_t size, float stepSize, std::size_t chunkSize){
+		_size = size;
+		_stepSize = stepSize;
+		_chunkSize = chunkSize;
 		
-			std::size_t chunksCount = size / chunkSize;
-			_chunks.reserve(chunksCount * chunksCount);
+		std::size_t chunksCount = size / chunkSize;
+		_chunks.reserve(chunksCount * chunksCount);
 
-			for(std::size_t x = 0; x < chunksCount; x++){
-				for(std::size_t z = 0; z < chunksCount; z++){
-					_chunks.emplace_back(x, z, stepSize, chunkSize, size);
-				}
+		for(std::size_t x = 0; x < chunksCount; x++){
+			for(std::size_t z = 0; z < chunksCount; z++){
+				_chunks.emplace_back(x, z, stepSize, chunkSize, size);
+			}
+		}
+	}
+
+	~Terrain(){
+		if(_sharedEBO) glDeleteBuffers(1, &_sharedEBO);
+	}
+		
+	void generate(){
+		pGenerateSharedEBO();
+
+		for(std::size_t i = 0; i < _chunks.size(); i++){
+			_chunks[i].generate(_sharedEBO, _sharedIndices.size());
+			_vertexCount += _chunks[i]._vertexCount;
+			_triangleCount += _sharedIndices.size() / 3;
+		}	
+
+		_sharedIndices.clear();
+		_sharedIndices.shrink_to_fit();
+	}
+		
+	void draw() const {
+		for(std::size_t i = 0; i < _chunks.size(); i++){
+			_chunks[i].draw();
+		}
+	}
+
+private:
+	unsigned int _sharedEBO = 0;
+	std::vector<unsigned int> _sharedIndices;
+	std::vector<Chunk> _chunks;
+
+	void pGenerateSharedEBO(){
+		std::size_t axisVertexCount = std::size_t(_chunkSize / _stepSize) + 1;
+		std::size_t quadCount = (axisVertexCount - 1) * (axisVertexCount - 1);
+
+		_sharedIndices.resize(quadCount * 6);
+
+		std::size_t index = 0;
+		for(std::size_t i = 0; i < axisVertexCount - 1; i++){
+			for(std::size_t j = 0; j < axisVertexCount - 1; j++){
+				std::size_t row1 = std::size_t(i * axisVertexCount + j);
+				std::size_t row2 = std::size_t((i + 1) * axisVertexCount + j);
+
+				_sharedIndices[index++] = row1;
+				_sharedIndices[index++] = row1 + 1;
+				_sharedIndices[index++] = row2;
+
+				_sharedIndices[index++] = row2;
+				_sharedIndices[index++] = row1 + 1;
+				_sharedIndices[index++] = row2 + 1;
 			}
 		}
 
-		~Terrain(){
-			if(_sharedEBO) glDeleteBuffers(1, &_sharedEBO);
-		}
-		
-		void generate(){
-			pGenerateSharedEBO();
-
-			for(std::size_t i = 0; i < _chunks.size(); i++){
-				_chunks[i].generate(_sharedEBO, _sharedIndices.size());
-				_vertexCount += _chunks[i]._vertexCount;
-				_triangleCount += _sharedIndices.size() / 3;
-			}	
-
-			_sharedIndices.clear();
-			_sharedIndices.shrink_to_fit();
-		}
-		
-		void draw() const {
-			for(std::size_t i = 0; i < _chunks.size(); i++){
-				_chunks[i].draw();
-			}
-		}
-
-	private:
-		unsigned int _sharedEBO = 0;
-		std::vector<unsigned int> _sharedIndices;
-		std::vector<Chunk> _chunks;
-
-		void pGenerateSharedEBO(){
-			std::size_t axisVertexCount = std::size_t(_chunkSize / _stepSize) + 1;
-			std::size_t quadCount = (axisVertexCount - 1) * (axisVertexCount - 1);
-
-			_sharedIndices.resize(quadCount * 6);
-
-			std::size_t index = 0;
-			for(std::size_t i = 0; i < axisVertexCount - 1; i++){
-				for(std::size_t j = 0; j < axisVertexCount - 1; j++){
-					std::size_t row1 = std::size_t(i * axisVertexCount + j);
-					std::size_t row2 = std::size_t((i + 1) * axisVertexCount + j);
-
-					_sharedIndices[index++] = row1;
-					_sharedIndices[index++] = row1 + 1;
-					_sharedIndices[index++] = row2;
-
-					_sharedIndices[index++] = row2;
-					_sharedIndices[index++] = row1 + 1;
-					_sharedIndices[index++] = row2 + 1;
-				}
-			}
-
-			glCreateBuffers(1, &_sharedEBO);
-			glNamedBufferData(_sharedEBO, _sharedIndices.size() * sizeof(unsigned int), _sharedIndices.data(), GL_STATIC_DRAW);
-		}
+		glCreateBuffers(1, &_sharedEBO);
+		glNamedBufferData(_sharedEBO, _sharedIndices.size() * sizeof(unsigned int), _sharedIndices.data(), GL_STATIC_DRAW);
+	}
 };
